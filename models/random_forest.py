b17d38a1df6d4806ec00575129c7ba3be707fa1d
Kenza Tazi <ktazi100@yahoo.co.uk>
2023-01-31 19:07:49 +0000

    typos

--- models/random_forest.py
+++ models/random_forest.py
@@ -1,21 +1,28 @@
 from sklearn.ensemble import RandomForestClassifier
-from utils.dataprep as dp
+import utils.dataprep as dp
 from utils import metrics
+import numpy as np
+import joblib
 
 
 class RandomForest(RandomForestClassifier):
-    """ Sci Kit Learn model initalised with our """
-    def __init__(n_estimators = 500, max_depth = 10, class_weight = "balanced_subsample", random_state = 0)
+    """ SciKit Learn model initalised with paper hyperparameters"""
+    def __init__(self, n_estimators=500, max_depth=10, class_weight="balanced_subsample", random_state=0):
         self.n_estimators = n_estimators
         self.max_depth = max_depth
         self.class_weight = class_weight
-        self.random_state = random state
+        self.random_state = random_state
 
 
 if __name__ in "___main__":
-    x_train, ytrain, x_test, y_test = dp.load_rfdata()
-    clf = RandomForest()
-    clf.fit(x_train, y_train)
-    y_pred = clf.predict()
-    auc = metrics.auc(y_test, y_pred)
+    # Load datacubes (you can dowload these from the Pyrocast database)
+    geo_cubes = np.load('')
+    env_cubes = np.load('')
+    flag_cubes = np.load('')
+    xtrain, ytrain, xtest, ytest = dp.load_rfdata()
+    rf = RandomForest()
+    rf.fit(xtrain, ytrain)
+    joblib.dump(rf, 'models/instances/test_rf.joblib')
+    ypred = rf.predict()
+    auc = metrics.auc(ytest, ypred)
     print(auc)
